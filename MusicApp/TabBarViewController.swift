d948fc8560dc20c37ee19c6b8f96fe33736b098d
Sergei Ivanov <100bb100hands@gmail.com>
2021-01-05 14:31:58 +0300

    add tab bar navigation

---
+++ MusicApp/TabBarViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainTabBar.swift
+//  MusicApp
+//
+//  Created by Сергей Иванов on 05.01.2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        tabBar.tintColor = .purple
+    
+        viewControllers = [
+            generateViewController(rootViewController: SearchViewController(), image: UIImage(systemName: "magnifyingglass")!, title: "Search"),
+            generateViewController(rootViewController: LibraryViewController(), image: UIImage(systemName: "music.note.list")!, title: "Library")
+        ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UINavigationController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.navigationBar.topItem?.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        return navigationVC
+    }
+
+}
