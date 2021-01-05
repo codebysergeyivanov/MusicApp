2b87e3cd91d74bbe8d07cf2033bfb835b583e83f
Sergei Ivanov <100bb100hands@gmail.com>
2021-01-05 11:49:47 +0300

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Сергей Иванов on 05.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
